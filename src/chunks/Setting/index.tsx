0d2487f959dfe8667e12e1f629a5623a447e5e9d
boboan <360969885@qq.com>
2021-08-23 01:18:24 +0800

    规范host config

--- src/chunks/Setting/index.tsx
+++ src/chunks/Setting/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
 
-export default function () {
+export default function Setting() {
     return <div>我是设置啊</div>
 }
--- src/utils/Request/hosts.ts
+++ src/utils/Request/hosts.ts
@@ -1,1 +1,32 @@
-export const HOST_REALWORLD = 'https://conduit.productionready.io/api'
+const NETWORK_ENV = process.env.NETWORK_ENV as Envs
+
+// 环境变量可选值
+type Envs = 'DEVELOP' | 'TEST' | 'UAT' | 'RELEASE'
+
+// 服务地址列表
+type Servers =
+    'realWorldServer' |
+    'otherServer'
+
+type ServerList = Record<Servers, string>
+
+const config: Record<Envs, ServerList> = {
+    DEVELOP: {
+        realWorldServer: 'https://conduit.productionready.io/api',
+        otherServer: '试一下'
+    },
+    TEST: {
+        realWorldServer: 'https://conduit.productionready.io/api',
+        otherServer: '试一下'
+    },
+    UAT: {
+        realWorldServer: 'https://conduit.productionready.io/api',
+        otherServer: '试一下'
+    },
+    RELEASE: {
+        realWorldServer: 'https://conduit.productionready.io/api',
+        otherServer: '试一下'
+    }
+}
+
+export default config[NETWORK_ENV]
