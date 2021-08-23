018751fdf8b1fa9df64c317c109c3d8db329f132
boboan <360969885@qq.com>
2021-08-23 23:04:59 +0800

    legacy ie 11 支持

--- build/build.conf.ts
+++ build/build.conf.ts
@@ -1,14 +1,15 @@
-import legacy from '@vitejs/plugin-legacy'
+import legacy from '@vitejs/plugin-legacy';
 
 export default (config) => ({
-    ...config,
-    plugins: [
-        ...config.plugins || [],
-        legacy({
-            targets: ['defaults', 'not IE 11']
-        })
-    ],
-    build: {
-        target: 'es2015'
-    }
-})
+	...config,
+	plugins: [
+		...(config.plugins || []),
+		legacy({
+			targets: ['ie >= 11'],
+			additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
+		}),
+	],
+	build: {
+		target: 'es2015',
+	},
+});
